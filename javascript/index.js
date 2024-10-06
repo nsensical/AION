b1d7745f2bee434f4ba13bafb2550f676e149d40
nsensical <iss.doli@gmail.com>
2024-10-06 16:25:16 -0400

    edited javascript
    added time zones + changed text transform property in css (so the text always shows up uppercased)

--- javascript/index.js
+++ javascript/index.js
@@ -2,37 +2,64 @@
 function updateTime() {
   // Caracas
   let caracasElement = document.querySelector("#caracas");
-  let caracasDateElement = caracasElement.querySelector(".date");
-  let caracasTimeElement = caracasElement.querySelector(".time");
-  let caracasTime = moment().tz("America/Caracas");
+  if (caracasElement) {
+    let caracasDateElement = caracasElement.querySelector(".date");
+    let caracasTimeElement = caracasElement.querySelector(".time");
+    let caracasTime = moment().tz("America/Caracas");
 
-  caracasDateElement.innerHTML = caracasTime.format("MMMM Do, YYYY");
-  caracasTimeElement.innerHTML = caracasTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    caracasDateElement.innerHTML = caracasTime.format("MMMM Do, YYYY");
+    caracasTimeElement.innerHTML = caracasTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Dublin
 
   let dublinElement = document.querySelector("#dublin");
-  let dublinDateElement = dublinElement.querySelector(".date");
-  let dublinTimeElement = dublinElement.querySelector(".time");
-  let dublinTime = moment().tz("Europe/Dublin");
+  if (dublinElement) {
+    let dublinDateElement = dublinElement.querySelector(".date");
+    let dublinTimeElement = dublinElement.querySelector(".time");
+    let dublinTime = moment().tz("Europe/Dublin");
 
-  dublinDateElement.innerHTML = dublinTime.format("MMMM Do, YYYY");
-  dublinTimeElement.innerHTML = dublinTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    dublinDateElement.innerHTML = dublinTime.format("MMMM Do, YYYY");
+    dublinTimeElement.innerHTML = dublinTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Dubai
 
   let dubaiElement = document.querySelector("#dubai");
-  let dubaiDateElement = dubaiElement.querySelector(".date");
-  let dubaiTimeElement = dubaiElement.querySelector(".time");
-  let dubaiTime = moment().tz("Asia/Dubai");
+  if (dubaiElement) {
+    let dubaiDateElement = dubaiElement.querySelector(".date");
+    let dubaiTimeElement = dubaiElement.querySelector(".time");
+    let dubaiTime = moment().tz("Asia/Dubai");
 
-  dubaiDateElement.innerHTML = dubaiTime.format("MMMM Do, YYYY");
-  dubaiTimeElement.innerHTML = dubaiTime.format("h:mm:ss [<small>]A[</small>]");
+    dubaiDateElement.innerHTML = dubaiTime.format("MMMM Do, YYYY");
+    dubaiTimeElement.innerHTML = dubaiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+      </div>
+      <div class="time">
+        ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
+      </div>
+    </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
